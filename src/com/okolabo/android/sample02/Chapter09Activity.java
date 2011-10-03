b5203ebb1f006c78ba32b6c9d8a4ad81a7a561f2
patorash <chariderpato@gmail.com>
2011-10-03 17:40:39 +0900

    first commit

---
+++ src/com/okolabo/android/sample02/Chapter09Activity.java
@@ -0,0 +1,51 @@
+package com.okolabo.android.sample02;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class Chapter09Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chapter09);
+        setTitle(R.string.title_chapter09);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch(item.getItemId()) {
+            case R.id.menu_chapter07:
+                intent = new Intent(this, Chapter07Activity.class);
+                startActivity(intent);
+                break;
+                
+            case R.id.menu_chapter08:
+                intent = new Intent(this, Chapter08Activity.class);
+                startActivity(intent);
+                break;
+                
+            case R.id.menu_chapter09:
+                intent = new Intent(this, Chapter09Activity.class);
+                startActivity(intent);
+                break;
+                
+            case R.id.menu_chapter10:
+                intent = new Intent(this, Chapter10Activity.class);
+                startActivity(intent);
+                break;
+        }
+        return true;
+    }
+}
